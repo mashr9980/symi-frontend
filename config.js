bfc1f65e3004e1d4ddb6c749ed6904534204b220
72MHz <waqassarwar780@gmail.com>
2025-05-18 13:28:51 +0500

    change flow and add 1st chat without login

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://localhost:7119',
+  apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.symi.io',
   webSocketUrl: 'ws://9640-175-107-235-139.ngrok-free.app/chat', // WebSocket URL
   googleClientId: "506721369558-hlbudtj8s2vdieg3v78r1g7f0l43p8i3.apps.googleusercontent.com",
   googleClientSecret: "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",
